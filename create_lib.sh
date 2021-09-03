d1de5af18b2a2440321f18cd47c1a86ba1725cb6
Daniel Gehrer <xa17d@users.noreply.github.com>
2021-09-03 10:25:58 +0200

    Add library creation script

---
+++ create_lib.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+libraryName=$1
+libraryVersion=$2
+discoveryDocumentUrl=$3
+
+if [[ -z "$libraryName" || -z "$libraryVersion" || -z "$discoveryDocumentUrl" ]]; then
+  echo "Parameters: libraryName libraryVersion discoveryDocumentUrl"
+  echo "See README.md for more details."
+  exit 1
+fi
+
+echo "libraryName:          $libraryName"
+echo "libraryVersion:       $libraryVersion"
+echo "discoveryDocumentUrl: $discoveryDocumentUrl"
+
+# make folder structure
+
+libraryFolder="libs/${libraryName}/${libraryVersion}"
+mkdir -p $libraryFolder
+
+# pull discovery document
+
+echo ""
+echo "▶️ Download discovery document..."
+
+curl --silent --output "${libraryFolder}/discoveryDocument.json" --write-out "download status: %{http_code}" --fail "${discoveryDocumentUrl}" || exit 1
+echo ""
+
+# generate sources using Docker
+
+echo "▶️ Copy files to make them available in Docker container..."
+
+containerFiles="${libraryFolder}/container_files"
+cp -r src/container_files "${containerFiles}"
+
+echo ""
+echo "▶️ Setting up Docker container..."
+
+containerIdFile="${PWD}/${libraryFolder}/container_id"
+docker run --cidfile "${containerIdFile}" -v "${PWD}/${libraryFolder}:/home/data" python:2 /home/data/container_files/install.sh
+containerId=`cat "${containerIdFile}"`
+
+echo "▶️ Deleting Docker container (containerId=${containerId})..."
+
+docker stop "${containerId}"
+docker rm -v "${containerId}"
+
+rm "${containerIdFile}"
+rm -rf "${libraryFolder}/container_files"
+
+echo "▶️ Done."
---
+++ src/container_files/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is meant to be run inside the Python2-Docker Container (see `create_lib.sh`)
+
+pip install google-apis-client-generator
+
+echo "▶️ Generating library..."
+cd /home/data
+generate_library --input=discoveryDocument.json --language=java --output_dir=.
